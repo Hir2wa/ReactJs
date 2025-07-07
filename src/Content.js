38f54c41462d45b98f463f2cb6ef7eef97317512
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-07 20:04:39 +0200

    added first click event

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,17 @@ const Content = () => {
     let init = Math.floor(Math.random() * 3);
     return name[init];
   };
+
+  const handleClick1 = () => {
+    console.log("clicked me ");
+  };
+  const handleClick2 = () => {
+    console.log("clicked me ");
+  };
   return (
     <main>
       <p>Hello {handleNameChanges()}</p>
+      <button onClick={handleClick1}>Add</button>
     </main>
   );
 };
