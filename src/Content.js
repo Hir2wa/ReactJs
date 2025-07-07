ba7d0adca8d3ea4834d06ace34005634c58bf186
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-07 22:08:57 +0200

    added click event

--- src/Content.js
+++ src/Content.js
@@ -10,6 +10,9 @@ const Content = () => {
   const handleClick1 = () => {
     console.log("clicked me ");
   };
+  const handleClick3 = (e) => {
+    console.log(e);
+  };
   const handleClick2 = (name) => {
     console.log(`${name} was clicked`);
   };
@@ -18,6 +21,7 @@ const Content = () => {
       <p>Hello {handleNameChanges()}</p>
       <button onClick={handleClick1}>Add</button>
       <button onClick={() => handleClick2("Alain")}>Add</button>
+      <button onClick={(e) => handleClick3(e)}>Add</button>
     </main>
   );
 };
