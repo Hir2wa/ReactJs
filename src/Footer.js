b040821ae32dc4f3c48f546f04ca375bd5f3d644
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 23:22:11 +0200

    added commmenting items with  templete literals

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = ({ length, items }) => {
   const today = new Date();
 
   return (
     <footer>
+      <p>
+        {length} List {items.length === 1 ? "item" : "items"}
+      </p>
       <p> Copyright &copy; {today.getFullYear()}</p>
     </footer>
   );
