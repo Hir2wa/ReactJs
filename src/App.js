66e17ab35bd38d66bb1d70e096e93abd8b0e265d
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 11:56:50 +0200

    imported ListKeys Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import ListKeys from "./ListKeys";
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
       <Footer />
+      <ListKeys />
     </div>
   );
 }
