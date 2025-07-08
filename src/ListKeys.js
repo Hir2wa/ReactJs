e32d4376a72337c3a521b99bce3f1a455e5757f0
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 12:39:15 +0200

    added list item on the list

--- src/ListKeys.js
+++ src/ListKeys.js
@@ -2,11 +2,21 @@ import React, { useState } from "react";
 
 const ListKeys = () => {
   const [items, setItems] = useState([
-    { id: 1, name: "Alain", checked: true },
-    { id: 2, name: "Hirwa", checked: false },
-    { id: 3, name: "Fabrice", checked: true },
+    { id: 1, item: "shoes", checked: true },
+    { id: 2, item: "t-shirt", checked: false },
+    { id: 3, item: "jacket", checked: true },
   ]);
-  return <main></main>;
+  return (
+    <main>
+      {items.map((item) => (
+        <li className="Items">
+          <input type="checkbox" checked={item.checked}></input>
+          <label>{item.item}</label>
+          <button>Delete</button>
+        </li>
+      ))}
+    </main>
+  );
 };
 
 export default ListKeys;
