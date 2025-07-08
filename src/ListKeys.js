5dd0ba821f1a89ee6e560ee153ba78b6cd735a1f
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 14:35:49 +0200

    added new changes

--- src/ListKeys.js
+++ src/ListKeys.js
@@ -8,12 +8,21 @@ const ListKeys = () => {
   ]);
 
   const handleChange = (id) => {
-    console.log(id);
+    const listItems = items.map((item) =>
+      item.id === id
+        ? {
+            ...item,
+            checked: !item.checked,
+          }
+        : item
+    );
+    setItems(listItems);
+    localStorage.setItem("Item-List", JSON.stringify(listItems));
   };
   return (
     <main>
       {items.map((item) => (
-        <li className="Items">
+        <li className="Items" key={item.id}>
           <input
             type="checkbox"
             checked={item.checked}
