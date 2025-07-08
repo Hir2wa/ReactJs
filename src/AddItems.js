ba3d2a3b1b249655569dda1bfc3826f73d8661df
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 23:36:33 +0200

    added form for input

--- src/AddItems.js
+++ src/AddItems.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const AddItems = () => {
   return (
-   
-  )
-}
+    <form>
+      <label htmlFor="addItem">Add Item</label>
+      <input
+        autoFocus
+        type="text"
+        id="addItem"
+        placeholder="Add-Item"
+        required
+      ></input>
+    </form>
+  );
+};
