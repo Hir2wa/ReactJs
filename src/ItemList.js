592c2d7935e8375366f9202081888d05e652a4a5
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 23:23:22 +0200

    defined item component List

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,27 +1,23 @@
 import React from "react";
-
-export const ItemList = () => {
-  return (
-    <ul>
-      {items.length ? (
-        items.map((item) => (
-          <li className="Items" key={item.id}>
-            <input
-              type="checkbox"
-              checked={item.checked}
-              onChange={() => handleChange(item.id)}
-            ></input>
-            <label>{item.item}</label>
-            <FaTrashAlt
-              role="button"
-              tabIndex={"0"}
-              onClick={() => handleDelete(item.id)}
-            />
-          </li>
-        ))
-      ) : (
-        <p style={{ marginTop: "2rem" }}> your list is empty</p>
-      )}
-    </ul>
+import { FaTrashAlt } from "react-icons/fa";
+export const ItemList = ({ items, handleChange, handleDelete }) => {
+  return items.length ? (
+    items.map((item) => (
+      <li className="Items" key={item.id}>
+        <input
+          type="checkbox"
+          checked={item.checked}
+          onChange={() => handleChange(item.id)}
+        ></input>
+        <label>{item.item}</label>
+        <FaTrashAlt
+          role="button"
+          tabIndex={"0"}
+          onClick={() => handleDelete(item.id)}
+        />
+      </li>
+    ))
+  ) : (
+    <p style={{ marginTop: "2rem" }}> your list is empty</p>
   );
 };
