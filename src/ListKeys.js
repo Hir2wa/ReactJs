24f16baed6731892878c58c423b7a428fc88836c
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 15:16:22 +0200

    validated list

--- src/ListKeys.js
+++ src/ListKeys.js
@@ -25,21 +25,25 @@ const ListKeys = () => {
   };
   return (
     <main>
-      {items.map((item) => (
-        <li className="Items" key={item.id}>
-          <input
-            type="checkbox"
-            checked={item.checked}
-            onChange={() => handleChange(item.id)}
-          ></input>
-          <label>{item.item}</label>
-          <FaTrashAlt
-            role="button"
-            tabIndex={"0"}
-            onClick={() => handeDelete(item.id)}
-          />
-        </li>
-      ))}
+      {items.length ? (
+        items.map((item) => (
+          <li className="Items" key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => handleChange(item.id)}
+            ></input>
+            <label>{item.item}</label>
+            <FaTrashAlt
+              role="button"
+              tabIndex={"0"}
+              onClick={() => handeDelete(item.id)}
+            />
+          </li>
+        ))
+      ) : (
+        <p style={{ marginTop: "2rem" }}> your list is empty</p>
+      )}
     </main>
   );
 };
