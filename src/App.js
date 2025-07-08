3a99219173348f6287cb7fba8d9f93fb511d7273
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-08 21:54:10 +0200

    added items props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Content from "./Content";
 import Footer from "./Footer";
 import ListKeys from "./ListKeys";
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, item: "shoes", checked: true },
+    { id: 2, item: "t-shirt", checked: false },
+    { id: 3, item: "jacket", checked: true },
+  ]);
+
   return (
     <div className="App">
       <Header title="Grocery List " />
