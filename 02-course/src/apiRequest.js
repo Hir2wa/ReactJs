4eed499a8657fa090a1c01ccea7200a4e9f4a258
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-11 23:45:35 +0200

    defined apiRequest obj

--- 02-course/src/apiRequest.js
+++ 02-course/src/apiRequest.js
@@ -1,1 +1,10 @@
-const apiRequest = (url = "", optObj = null, errorMsg = null) => {};
+const apiRequest = async (url = "", optObj = null, errorMsg = null) => {
+  try {
+    const response = await fetch(url, optObj);
+    if (!response.ok) throw Error("Refresh App");
+  } catch (error) {
+    errorMsg = error.message;
+  } finally {
+    return errorMsg;
+  }
+};
