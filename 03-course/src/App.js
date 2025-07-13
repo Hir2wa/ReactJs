26b34fb152643518f9849747e43c45afad7437e8
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-13 10:36:34 +0200

    added imported Home & NewPost component

--- 03-course/src/App.js
+++ 03-course/src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+
 function App() {
   return <p>Hello</p>;
 }
