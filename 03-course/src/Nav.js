5a932caad3b00570c419bdf187ac30d8ddf7fde8
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-13 13:02:04 +0200

    created controlled input using state

--- 03-course/src/Nav.js
+++ 03-course/src/Nav.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Nav = () => {
+  const [search, setSearch] = useState("");
   return (
-    <nav>
-      <p>Nav</p>
+    <nav className="Nav">
+      <form className="searchForm" onSubmit={(e) => e.preventDefault}>
+        <label form="search"> Search-Item </label>
+        <input
+          id="search"
+          placeholder="Search-Posts"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
     </nav>
   );
 };
