58c275a4e64ccb612d7d44db0b6727942d03a623
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-14 23:17:36 +0200

    added space for good readability

--- 04-course/src/App.tsx
+++ 04-course/src/App.tsx
@@ -3,15 +3,12 @@ import { Section } from "./components/Section";
 import { useState } from "react";
 import { Counter } from "./components/Counter";
 
-const [counter, setCounter] = useState<number>(1);
 function App() {
+  const [counter, setCounter] = useState<number>(1);
   return (
     <>
-      <Heading title="Hello World" />
-      <Section title="These is not my default title3">
-        {" "}
-        these is my section{" "}
-      </Section>
+      <Heading title=".." />
+      <Section title="...">....</Section>
       <Counter setCounter={setCounter}>{counter}</Counter>
     </>
   );
