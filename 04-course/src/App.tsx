149709a91d7cc22e3abe4a283241b44d4fd077c4
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-14 15:49:43 +0200

    added override default title

--- 04-course/src/App.tsx
+++ 04-course/src/App.tsx
@@ -1,6 +1,15 @@
 import { Heading } from "./components/Heading";
+import { Section } from "./components/Section";
 function App() {
-  return <Heading title="Hello World" />;
+  return (
+    <>
+      <Heading title="Hello World" />
+      <Section title="These is not my default title3">
+        {" "}
+        these is my section{" "}
+      </Section>
+    </>
+  );
 }
 
 export default App;
