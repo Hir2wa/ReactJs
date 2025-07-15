dd2bf906a5c2204b27ffa5999a78397e0ce99a84
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-15 15:12:34 +0200

    added  list component on the App.tsx

--- 04-course/src/App.tsx
+++ 04-course/src/App.tsx
@@ -2,7 +2,7 @@ import { Heading } from "./components/Heading";
 import { Section } from "./components/Section";
 import { useState } from "react";
 import { Counter } from "./components/Counter";
-
+import { List } from "./components/List";
 function App() {
   const [counter, setCounter] = useState<number>(1);
   return (
@@ -10,6 +10,10 @@ function App() {
       <Heading title=".." />
       <Section title="...">....</Section>
       <Counter setCounter={setCounter}>{counter}</Counter>
+      <List
+        items={["cofffe", "tocs", "mild"]}
+        render={(item: string) => <span className="food"> {item}</span>}
+      />
     </>
   );
 }
