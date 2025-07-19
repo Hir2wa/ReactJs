76c97591d49c32b44b9341c1568daa20556ebbd9
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-19 23:15:38 +0200

    removed = error to define property  put : as one used

--- 05-course/src/ReducerHook.js
+++ 05-course/src/ReducerHook.js
@@ -1,8 +1,11 @@
 import React, { useReducer } from "react";
-
+import {ReactNode} from "react"
 function reducer(state, action) {}
 export const ReducerHook = () => {
   const [state, dipatch] = useReducer(reducer, initialState);
+  type childrenType = {
+    children = (num: number)=> ReactNode
+  };
   const initialState = { count: 0 };
   return <div>ReducerHook</div>;
 };
