733de7493873ed2e71a34214d24b9c6d429ffca7
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-19 23:05:43 +0200

    added new changes

--- 05-course/src/ReducerHook.js
+++ 05-course/src/ReducerHook.js
@@ -3,6 +3,6 @@ import React, { useReducer } from "react";
 function reducer(state, action) {}
 export const ReducerHook = () => {
   const [state, dipatch] = useReducer(reducer, initialState);
-
+  //  these usReducer  act as the reducer method in the array prototype
   return <div>ReducerHook</div>;
 };
