fc4f3957199839e0a2f48d12de518ddadac567e4
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-19 23:09:45 +0200

    started working with enums+ useReducer

--- 05-course/src/ReducerHook.js
+++ 05-course/src/ReducerHook.js
@@ -3,6 +3,6 @@ import React, { useReducer } from "react";
 function reducer(state, action) {}
 export const ReducerHook = () => {
   const [state, dipatch] = useReducer(reducer, initialState);
-
+  const initialState = { count: 0 };
   return <div>ReducerHook</div>;
 };
