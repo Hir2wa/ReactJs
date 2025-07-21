d13493efe9d38fb5799c2ab4894897419b5a481f
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-21 13:53:48 +0200

    used pasted codes from ReducerHook

---
+++ 05-course/src/context/CounterContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, type ReactNode } from "react";
+
+const initState = { count: 0, text: "" };
+
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT,
+  NEW_INPUT,
+}
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE;
+  payload?: string;
+};
+
+type childrenType = {
+  children: (count: number) => ReactNode;
+};
+const reducer = (
+  state: typeof initState,
+  action: ReducerAction
+): typeof initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 };
+
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case REDUCER_ACTION_TYPE.NEW_INPUT:
+      return { ...state, text: action.payload ?? "" };
+    default:
+      throw new Error("Error Encounted");
+  }
+};
