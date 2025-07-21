f751e6f5f75d0086ba65fed21e8074df55e9399b
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-21 13:00:54 +0200

    created childNode which return a function type

--- 05-course/src/ReducerHook.tsx
+++ 05-course/src/ReducerHook.tsx
@@ -4,39 +4,44 @@ import React, { Children, useReducer, type ReactNode } from "react";
 
 const initialState = { count: 0 };
 
-
-const initState = { count: 0}
+const initState = { count: 0 };
 const enum REDUCER_ACTION_TYPE {
   INCREMENT,
   DECREMENT,
-} 
- type ReducerAction  = {
-  type: REDUCER_ACTION_TYPE
 }
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE;
+};
 
-  const reducer =(state: typeof initState , action: ReducerAction
-) : typeof initState  =>{
+type childrenType = {
+  children: (count: number) => ReactNode;
+};
+const reducer = (
+  state: typeof initState,
+  action: ReducerAction
+): typeof initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 };
 
-  switch (action.type){
-
-    case REDUCER_ACTION_TYPE.INCREMENT :
-      return {...state , count: state.count + 1}
-
-      case REDUCER_ACTION_TYPE.DECREMENT :
-        return {...state , count: state.count + 1}
-        default: 
-        throw new Error("Error Encounted")
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count + 1 };
+    default:
+      throw new Error("Error Encounted");
   }
- 
-}
-const [state, despatch] = useReducer(reducer, initState)
-const increment = ()=> despatch({type: REDUCER_ACTION_TYPE.INCREMENT})
-const decrememt = ()=> despatch({type: REDUCER_ACTION_TYPE.DECREMENT})
+};
+const [state, despatch] = useReducer(reducer, initState);
+const increment = () => despatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
+const decrememt = () => despatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
 
-export const ReducerHook = () => {
-  
-
+export const ReducerHook = ({ children }: childrenType) => {
   return (
-  
+    <>
+      <h1>{children(state.count)}</h1>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrememt}>-</button>
+      </div>
+    </>
   );
 };
