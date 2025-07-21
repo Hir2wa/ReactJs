c4b5aee717d9c7bb71f594722bb73cd068dd967d
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-21 12:27:49 +0200

    added some changes

--- 05-course/src/ReducerHook.tsx
+++ 05-course/src/ReducerHook.tsx
@@ -1,46 +1,23 @@
-import React, { useReducer, type ReactNode } from "react";
+import React, { Children, useReducer, type ReactNode } from "react";
 
 // ✅ Using const + union instead of enum
-const REDUCER_ACTION_TYPE = {
-  INCREMENT: "INCREMENT",
-  DECREMENT: "DECREMENT",
-} as const;
-
-type REDUCER_ACTION_TYPE =
-  (typeof REDUCER_ACTION_TYPE)[keyof typeof REDUCER_ACTION_TYPE];
 
 const initialState = { count: 0 };
 
-function reducer(
-  state: typeof initialState,
-  action: { type: REDUCER_ACTION_TYPE }
-) {
-  switch (action.type) {
-    case REDUCER_ACTION_TYPE.INCREMENT:
-      return { count: state.count + 1 };
-    case REDUCER_ACTION_TYPE.DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
+function reducer() {}
+const initState = { count: 0}
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT,
+} 
+ type ReducerAction  = {
+  type: REDUCER_ACTION_TYPE
 }
 
-type childrenType = {
-  children: (num: number) => ReactNode;
-};
-
 export const ReducerHook = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  
 
   return (
-    <div>
-      <p>Count: {state.count}</p>
-      <button onClick={() => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT })}>
-        +
-      </button>
-      <button onClick={() => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT })}>
-        -
-      </button>
-    </div>
+  
   );
 };
