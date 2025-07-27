a4dd642f229de075071cb84f2240f9e13a847bce
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-27 15:05:44 +0200

    added context component

--- 04-course/src/App.tsx
+++ 04-course/src/App.tsx
@@ -4,17 +4,21 @@ import { useState } from "react";
 import { Counter } from "./components/Counter";
 import { List } from "./components/List";
 import { ComponentA } from "./ComponentA";
+import { createContext } from "react";
+export const UserContext = createContext();
 function App() {
+  const [user, setUser] = useState("Alain");
   const [counter, setCounter] = useState<number>(1);
   return (
     <>
-      <Heading title=".." />
+      {/* <Heading title=".." />
       <Section title="...">....</Section>
       <Counter setCounter={setCounter}>{counter}</Counter>
       <List
         items={["cofffe", "tocs", "milk"]}
         render={(item: string) => <span className="food"> {item}</span>}
-      />
+      /> */}
+      <UserContext.Provider value={user}></UserContext.Provider>
     </>
   );
 }
