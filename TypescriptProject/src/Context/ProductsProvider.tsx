cbc3bc45981a87f319e3aa82bb2f2444ca207816
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-29 14:18:26 +0200

    created Promise inside the  provider function

--- TypescriptProject/src/Context/ProductsProvider.tsx
+++ TypescriptProject/src/Context/ProductsProvider.tsx
@@ -24,6 +24,14 @@ export type ProductType = {
 //   },
 // ];
 
+const initState: ProductType[] = []
+
+
+
+
+
+
+
 export type UseProductContextType = { product: ProductType[] };
 const initContextState: UseProductContextType = { product: [] };
 const ProductsContext = createContext<UseProductContextType>(initContextState);
@@ -34,6 +42,17 @@ type ChildrenType = {
 
 export const ProductProvider = ({ children }: ChildrenType): ReactElement => {
   const [products, setProducts] = useState<ProductType[]>(initState);
+
+   useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+     const data = await fetch('http://localhost:3500/products').then(res=>  res.json()).catch(err=>{
+      if(err instanceof Error ) console.log(err.message);
+     })
+     return data
+    }
+
+   fetchProducts().then(products => setProducts(products))
+   })
   return (
     <ProductsContext.Provider value={{ products }}>
       {children}
