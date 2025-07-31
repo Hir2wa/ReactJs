d69072ef009cf95ed28f390df9a328c2f6e4960a
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-31 11:00:51 +0200

    drag the function inside the custom component

--- TypescriptProject/src/Context/ProductLearn.tsx
+++ TypescriptProject/src/Context/ProductLearn.tsx
@@ -1,1 +1,32 @@
 //gona try my best to create a product context for now for sure
+
+import { createContext, useEffect, type ReactElement } from "react";
+
+//how to create a context
+
+type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+type ProductStateType = { product: ProductType[] };
+type ChildrenType = {
+  children?: ReactElement | ReactElement[];
+};
+
+const initProduct: ProductStateType = { product: [] };
+
+const ProductProvider = ({ children }: ChildrenType): ReactElement => {
+    const 
+  useEffect( () => {
+    const fetchData = async () =>{
+        const data = await fetch("https://localhost:5500/products").then(
+            (res) => res.json
+          );
+          return data;
+    }
+   
+  }, []);
+};
+
+export const ProductContext = createContext(initProduct);
