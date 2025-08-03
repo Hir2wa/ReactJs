943e995606d63bbe202e871857d0655ef86f969f
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 15:49:08 +0200

    imported propsTypes

--- TypescriptProject/src/components/Nav.tsx
+++ TypescriptProject/src/components/Nav.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+type propsType = {
+  viewCart: boolean;
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
+};
 const Nav = () => {
   return <div>Nav</div>;
 };
