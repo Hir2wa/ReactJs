cbe21711de43b569f3edc3c7ea66bfa932e279c7
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 16:39:47 +0200

    imported useCart custom hook

--- TypescriptProject/src/components/Header.tsx
+++ TypescriptProject/src/components/Header.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Nav from "./Nav";
+import useCart from "../hooks/useCart";
 type propsType = {
   viewCart: boolean;
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
 const Header = ({ viewCart, setViewCart }: propsType) => {
+  const { TotalItems, TotalPrice } = useCart();
   const content = (
     <header className="header">
       <div className="header__title-bar">
         <h1>Acme Co.</h1>
         <div className="header___price-box">
-          <p>Total Items:</p>
-          <p>Total Price:</p>
+          <p>Total Items: {TotalItems}</p>
+          <p>Total Price: {TotalPrice}</p>
         </div>
       </div>
       <Nav viewCart={viewCart} setViewCart={setViewCart} />
