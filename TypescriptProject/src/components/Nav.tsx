a36b9df52a2232e602fde72bf6892622cd640dda
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 16:00:49 +0200

    returned the value

--- TypescriptProject/src/components/Nav.tsx
+++ TypescriptProject/src/components/Nav.tsx
@@ -4,15 +4,14 @@ type propsType = {
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
 const Nav = ({ viewCart, setViewCart }: propsType) => {
-    const botton = viewCart ? <button onClick={()=>setViewCart(false)}> View Product</button>
-     : <button onClick={()=>setViewCart(true)}>View Cart</button>
-    
-    const content = (
-        <nav className="nav">{botton}</nav>
-    )
-  return (
+  const botton = viewCart ? (
+    <button onClick={() => setViewCart(false)}> View Product</button>
+  ) : (
+    <button onClick={() => setViewCart(true)}>View Cart</button>
+  );
 
-  )
+  const content = <nav className="nav">{botton}</nav>;
+  return { content };
 };
 
 export default Nav;
