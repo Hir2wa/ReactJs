8c8e2473dacbab6c134ee655be7bbe9ff4482c62
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 14:47:27 +0200

    commented learning codes

--- TypescriptProject/src/Context/ProductLearn.tsx
+++ TypescriptProject/src/Context/ProductLearn.tsx
@@ -1,39 +1,39 @@
-//gona try my best to create a product context for now for sure
+// //gona try my best to create a product context for now for sure
 
-import { createContext, useEffect, useState, type ReactElement } from "react";
+// import { createContext, useEffect, useState, type ReactElement } from "react";
 
-//how to create a context
+// //how to create a context
 
-type ProductType = {
-  sku: string;
-  name: string;
-  price: number;
-};
-type ProductStateType = { product: ProductType[] };
-type ChildrenType = {
-  children?: ReactElement | ReactElement[];
-};
+// type ProductType = {
+//   sku: string;
+//   name: string;
+//   price: number;
+// };
+// type ProductStateType = { product: ProductType[] };
+// type ChildrenType = {
+//   children?: ReactElement | ReactElement[];
+// };
 
-const initProduct: ProductType[] = [];
-export const ProductContext = createContext(initProduct);
+// const initProduct: ProductType[] = [];
+// export const ProductContext = createContext(initProduct);
 
-const ProductProvider = ({ children }: ChildrenType): ReactElement => {
-  const [products, setProducts] = useState<ProductType[]>(initProduct);
-  useEffect(() => {
-    const fetchData = async (): Promise<ProductType[]> => {
-      const data = await fetch("https://localhost:5500/products")
-        .then((res) => res.json())
-        .catch((err) => {
-          if (err instanceof Error) throw new Error("value not provided ");
-        });
-      return data;
-    };
-    fetchData().then((products) => setProducts(products));
-  }, []);
+// const ProductProvider = ({ children }: ChildrenType): ReactElement => {
+//   const [products, setProducts] = useState<ProductType[]>(initProduct);
+//   useEffect(() => {
+//     const fetchData = async (): Promise<ProductType[]> => {
+//       const data = await fetch("https://localhost:5500/products")
+//         .then((res) => res.json())
+//         .catch((err) => {
+//           if (err instanceof Error) throw new Error("value not provided ");
+//         });
+//       return data;
+//     };
+//     fetchData().then((products) => setProducts(products));
+//   }, []);
 
-  return (
-    <ProductContext.Provider value={products}>
-      {children}
-    </ProductContext.Provider>
-  );
-};
+//   return (
+//     <ProductContext.Provider value={products}>
+//       {children}
+//     </ProductContext.Provider>
+//   );
+// };
