5e3ab3fe501b88c54bba84748e689a31e14015b5
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 15:50:32 +0200

    added type component

--- TypescriptProject/src/components/Nav.tsx
+++ TypescriptProject/src/components/Nav.tsx
@@ -3,7 +3,7 @@ type propsType = {
   viewCart: boolean;
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
-const Nav = () => {
+const Nav = ({ viewCart, setViewCart }: propsType) => {
   return <div>Nav</div>;
 };
 
