4abbd2f380c6c37b57e5964b3c7001ebc3712ad7
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 15:11:32 +0200

    imported needed file

--- TypescriptProject/src/App.tsx
+++ TypescriptProject/src/App.tsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Product from "./components/Product";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
 import "./App.css";
 
 function App() {
