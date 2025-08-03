ff9a4731959e62211a7947a73ae1c1eba4eb5547
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-03 15:45:39 +0200

    added component discription

--- TypescriptProject/src/components/Header.tsx
+++ TypescriptProject/src/components/Header.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-
-const Header = () => {
-  return <div>Header</div>;
+type propsType = {
+  viewCart: boolean;
+  setViewCart: React.Dispatch<React.SetStateAction<boolean>>;
 };
+const Header = ({ viewCart, setViewCart }: propsType) => {
+    const content = (
+      <header className="header">
+        <div className="header__title-bar">
+            <h1>Acme Co.</h1>
+            <div className="header___price-box">
+                <p>Total Items:</p>
+                <p>Total Price:</p>
+            </div>
+        </div>
+      </header>
+);
 
 export default Header;
