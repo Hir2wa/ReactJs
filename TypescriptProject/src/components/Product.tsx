a13f87412b8a1a8939e85c17e9438529b28f0425
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-04 15:12:38 +0200

    imported

--- TypescriptProject/src/components/Product.tsx
+++ TypescriptProject/src/components/Product.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-const Product = () => {
+import type { ReducerActionType } from "../Context/CardProvider";
+import { ProductType } from "../Context/ProductsProvider";
+import { ReducerAction } from "../Context/CardProvider";
+const Product = ({}) => {
   return <div>Products</div>;
 };
 
