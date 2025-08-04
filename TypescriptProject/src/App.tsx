d7d9f1982d264755df4133165be53c46ce27989a
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-04 15:01:34 +0200

    defined useProducts hook

--- TypescriptProject/src/App.tsx
+++ TypescriptProject/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Product from "./components/Product";
+import Product from "./components/ProductsList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Cart from "./components/Cart";
