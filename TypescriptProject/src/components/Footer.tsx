383df16325b0ad39f542cee9cec3a0498a9c757e
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-04 13:17:10 +0200

    added new file for import

--- TypescriptProject/src/components/Footer.tsx
+++ TypescriptProject/src/components/Footer.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import useCart from "../hooks/useCart";
-const Footer = ({ viewCart }) => {
+
+type propsType = {
+  viewCart: boolean;
+};
+const Footer = ({ viewCart }: propsType) => {
+  const { TotalItems, TotalItems } = useCart();
+
   return <div>Footer</div>;
 };
 
