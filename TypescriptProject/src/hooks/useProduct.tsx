c6cb03f1ca4cce22c20971abb9a7b19346ec39bd
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-04 14:15:42 +0200

    imported types

--- TypescriptProject/src/hooks/useProduct.tsx
+++ TypescriptProject/src/hooks/useProduct.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { UseProductContextType } from "../Context/ProductsProvider";
+import ProductContext
 const useProduct = () => {
   return <div>useProduct</div>;
 };
