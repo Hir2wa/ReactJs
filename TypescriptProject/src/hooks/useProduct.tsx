2ac1fa4b5789a68a1da7a8b679018ba84ba9b561
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-04 14:18:26 +0200

    defined useProducts hook

--- TypescriptProject/src/hooks/useProduct.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { UseProductContextType } from "../Context/ProductsProvider";
-import ProductContext
-const useProduct = () => {
-  return <div>useProduct</div>;
-};
-
-export default useProduct;
