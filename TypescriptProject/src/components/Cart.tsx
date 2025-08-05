4f16e292acc205fbbfe2db9914b431d3bdcf1157
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-05 15:28:05 +0200

    added new changes

--- TypescriptProject/src/components/Cart.tsx
+++ TypescriptProject/src/components/Cart.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import useCart from "../hooks/useCart";
 import { useState } from "react";
+import CartLine from "./CartLine";
 const Cart = () => {
+  const [confirm, setConfirm] = useState<boolean>(false);
+  const { dispatch, REDUCER_ACTIONS, cart, TotalItems, TotalPrice } = useCart();
+  const onSubmitOrder = () => {
+    dispatch({ type: REDUCER_ACTIONS.SUBMIT });
+    setConfirm(true);
+  };
+
+  const content = confirm ? (
+    <h2>Thank You For Your Order....</h2>
+  ) : (
+    <>
+      <h2 className="offscreen">cart</h2>
+      <ul className="cart">
+        {cart.map((item) => {
+          return <CartLine />;
+        })}
+      </ul>
+    </>
+  );
   return <div>Cart</div>;
 };
 
