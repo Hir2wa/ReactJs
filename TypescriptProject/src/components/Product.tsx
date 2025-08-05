a4b66b1f156dcc6aac3b3d3929378b50029d8324
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-05 10:04:14 +0200

    added the urls to import the images

--- TypescriptProject/src/components/Product.tsx
+++ TypescriptProject/src/components/Product.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { type ReactElement } from "react";
 import type { ReducerActionType } from "../Context/CardProvider";
-import { ProductType } from "../Context/ProductsProvider";
-import { ReducerAction } from "../Context/CardProvider";
-const Product = ({}) => {
+import type { ProductType } from "../Context/ProductsProvider";
+import type { ReducerAction } from "../Context/CardProvider";
+
+type propsType = {
+  inCart: boolean;
+  dispatch: React.Dispatch<ReducerAction>;
+  REDUCER_ACTIONS: ReducerActionType;
+  product: ProductType;
+};
+const Product = ({
+  inCart,
+  dispach,
+  REDUCER_ACTIONS,
+  product,
+}: propsType): ReactElement => {
+  const images: string = new URL(
+    `../images/${product.sku}.jpg`,
+    import.meta.url
+  ).href;
+
   return <div>Products</div>;
 };
 
