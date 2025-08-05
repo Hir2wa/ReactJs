a6de9d6f997a25941f877cbd4d2f15a78cca01e7
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-05 16:41:16 +0200

    added new change

--- TypescriptProject/src/components/CartLine.tsx
+++ TypescriptProject/src/components/CartLine.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type ReactElement } from "react";
 import type { CartItemType } from "../Context/CardProvider";
 import type { ReducerAction } from "../Context/CardProvider";
 import type { ReducerActionType } from "../Context/CardProvider";
@@ -12,7 +12,16 @@ const CartLine = ({ item, dispatch, REDUCER_ACTIONS }: typeProps) => {
   const images: string = item?.sku
     ? new URL(`../images/${item.sku}.jpg`, import.meta.url).href
     : "/images/fallback.jpg"; // default fallback
-
+  const lineTotal: number = item.price * item.qty;
+  const hightqty: number = 20 > item.qty ? 20 : item.qty;
+  const optionValues: number[] = [...Array(hightqty)].map((i) => i + 1);
+  const options: ReactElement[] = optionValues.map((val) => {
+    return (
+      <option key={`opt${val}`} value={val}>
+        {val}
+      </option>
+    );
+  });
   return <div>CartLineComponent</div>;
 };
 
