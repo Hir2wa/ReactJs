3d4ce9c933c0a95fb1e9e8005c2ae80495701e02
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-05 11:08:11 +0200

    made same changes

--- TypescriptProject/src/components/Product.tsx
+++ TypescriptProject/src/components/Product.tsx
@@ -2,6 +2,7 @@ import React, { type ReactElement } from "react";
 import type { ReducerActionType } from "../Context/CardProvider";
 import type { ProductType } from "../Context/ProductsProvider";
 import type { ReducerAction } from "../Context/CardProvider";
+import { PiTicketThin } from "react-icons/pi";
 
 type propsType = {
   inCart: boolean;
@@ -11,7 +12,7 @@ type propsType = {
 };
 const Product = ({
   inCart,
-  dispach,
+  dispatch,
   REDUCER_ACTIONS,
   product,
 }: propsType): ReactElement => {
@@ -19,6 +20,28 @@ const Product = ({
     `../images/${product.sku}.jpg`,
     import.meta.url
   ).href;
+  console.log(images);
+  const addToCart = () =>
+    dispatch({
+      type: REDUCER_ACTIONS.ADD,
+      payload: { ...product, qty: 1 },
+    });
+
+  const itemInCart = inCart ? `✅ -> Item in cart" : "➕ Add to cart` : null;
+  const content = (
+    <article className="product">
+      <h3>{product.name}</h3>
+      <img src={images} alt={product.name} className="product__img" />
+      <p>
+        {new Intl.NumberFormat("en-Us", {
+          style: "currency",
+          currency: "USD",
+        }).format(product.price)}{" "}
+        {itemInCart}
+      </p>
+      <button onClick={addToCart}></button>
+    </article>
+  );
 
   return <div>Products</div>;
 };
