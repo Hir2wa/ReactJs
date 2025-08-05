ba4447c70ae56b117a0487104ef74fd62ccc7d0c
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-05 15:59:59 +0200

    imported new changes

--- TypescriptProject/src/components/CartLine.tsx
+++ TypescriptProject/src/components/CartLine.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import type { CartItemType } from "../Context/CardProvider";
+import type { ReducerAction } from "../Context/CardProvider";
+import type { ReducerActionType } from "../Context/CardProvider";
 
+type typeProps = {
+  item: CartItemType;
+  dispatch: React.Dispatch<React.SetStateAction<boolean>>;
+  REDUCER_ACTIONS: ReducerAction;
+};
 const CartLine = () => {
   return <div>CartLineComponent</div>;
 };
