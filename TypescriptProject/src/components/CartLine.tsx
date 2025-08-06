3ac4047990f5471514dcec9e0745fafc60dbd8be
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-06 15:14:22 +0200

    added defined component

--- TypescriptProject/src/components/CartLine.tsx
+++ TypescriptProject/src/components/CartLine.tsx
@@ -5,40 +5,43 @@ import type { ReducerActionType } from "../Context/CardProvider";
 
 type typeProps = {
   item: CartItemType;
-  dispatch: React.Dispatch<React.SetStateAction<boolean>>;
+  dispatch: React.Dispatch<ReducerAction>;
   REDUCER_ACTIONS: ReducerActionType;
 };
+
 const CartLine = ({ item, dispatch, REDUCER_ACTIONS }: typeProps) => {
   const images: string = item?.sku
     ? new URL(`../images/${item.sku}.jpg`, import.meta.url).href
-    : "/images/fallback.jpg"; // default fallback
+    : "/images/fallback.jpg";
+
   const lineTotal: number = item.price * item.qty;
   const hightqty: number = 20 > item.qty ? 20 : item.qty;
-  const optionValues: number[] = [...Array(hightqty)].map((i) => i + 1);
-  const options: ReactElement[] = optionValues.map((val) => {
-    return (
-      <option key={`opt${val}`} value={val}>
-        {val}
-      </option>
-    );
-  });
-  const onChangeqty = (e: ChangeEvent) => {
+  const optionValues: number[] = [...Array(hightqty).keys()].map((i) => i + 1);
+
+  const options: ReactElement[] = optionValues.map((val) => (
+    <option key={`opt${val}`} value={val}>
+      {val}
+    </option>
+  ));
+
+  const onChangeqty = (e: ChangeEvent<HTMLSelectElement>) => {
     dispatch({
       type: REDUCER_ACTIONS.QUANTITY,
       payload: { ...item, qty: Number(e.target.value) },
     });
   };
 
-  const onRemoveFromCar = () =>
+  const onRemoveFromCart = () =>
     dispatch({ type: REDUCER_ACTIONS.REMOVE, payload: item });
+
   const content = (
     <li className="cart__item">
       <img src={images} alt={item.name} className="cart__img" />
       <div aria-label="Item Name">{item.name}</div>
       <div aria-label="Price Per Item">
         {new Intl.NumberFormat("en-us", {
-          style = "currency",
-          currency = "USD",
+          style: "currency",
+          currency: "USD",
         }).format(item.price)}
       </div>
       <label htmlFor="itemQty" className="offscreen">
@@ -49,22 +52,30 @@ const CartLine = ({ item, dispatch, REDUCER_ACTIONS }: typeProps) => {
         id="itemQty"
         className="cart__select"
         value={item.qty}
-        area-label="Item Quantity"
+        aria-label="Item Quantity"
         onChange={onChangeqty}
       >
-        {" "}
         {options}
       </select>
-      <div className="cart__item-subtotal" area-label="Line Item Subtotal  ">
+      <div className="cart__item-subtotal" aria-label="Line Item Subtotal">
         {new Intl.NumberFormat("en-us", {
-          style = "currency",
-          currency = "USD",
-        }).format(item.price)}
+          style: "currency",
+          currency: "USD",
+        }).format(lineTotal)}
       </div>
+
+      <button
+        className="cart_button"
+        aria-label="Remove Item From Cart"
+        title="Remove Item From Cart"
+        onClick={onRemoveFromCart}
+      >
+        📕
+      </button>
     </li>
   );
 
-  return <div>CartLineComponent</div>;
+  return content;
 };
 
 export default CartLine;
