0fc818c39c0dc360a47829a8da1046a7c9b7f188
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-07 14:48:36 +0200

    development  file

---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,11 @@
+import ListGroup from "./components/ListGroup";
+function App() {
+  return (
+    <div className="m-10">
+      <h2>To Do List</h2>
+      <ListGroup />
+    </div>
+  );
+}
+
+export default App;
