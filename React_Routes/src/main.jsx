80e73e5cb993a4d55113f52775767e370fd98e6f
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-11 12:19:31 +0200

    made the main component as for rendering only

--- React_Routes/src/main.jsx
+++ React_Routes/src/main.jsx
@@ -2,16 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import About from "./About.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/About" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
