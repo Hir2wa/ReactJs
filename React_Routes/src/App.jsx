dfb8f3b88a4b2894f7033631f69c9fe5f8ad7c53
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-11 12:18:45 +0200

    made the App component as main for routing

--- React_Routes/src/App.jsx
+++ React_Routes/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-
+import { BrowserRouter, Router, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -9,7 +9,12 @@ function App() {
 
   return (
     <>
-      <p>Hello , React Router</p>
+      <BrowserRouter>
+        <Router>
+          <Routes path="/" element={<App />} />
+          <Routes path="/About" element={<About />} />
+        </Router>
+      </BrowserRouter>
     </>
   );
 }
