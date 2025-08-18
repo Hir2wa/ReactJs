9379feeda0a5f472fa43193c1695c70c98c88d34
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-18 18:23:02 +0200

    added  LayoutOut component

--- Exercises/src/App.jsx
+++ Exercises/src/App.jsx
@@ -5,7 +5,11 @@ import "./App.css";
 import Home from "./pages/Home";
 import { Route, Routes, Link } from "react-router-dom";
 import About from "./pages/About";
-import Books from "./pages/Books";
+import Book from "./pages/Book";
+import BooksList from "./pages/BooksList";
+import NewPage from "./pages/NewPage";
+import NotFound from "./pages/NotFound";
+import BookLayout from "./pages/BookLayout";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,13 +20,24 @@ function App() {
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/books">Books</Link>
           </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<Books />}></Route>
+        <Route path="/books" element={<BookLayout />}>
+          <Route index element={<BooksList />}></Route>
+          <Route path=":id" element={<Book />}></Route>
+          <Route path="new" element={<NewPage />}></Route>
+        </Route>
+
+        {/* <Route path="/book" element={<BooksList />}></Route>
+        <Route path="book/:id" element={<Book />}></Route>
+        <Route path="book/new" element={<NewPage />}></Route> */}
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
