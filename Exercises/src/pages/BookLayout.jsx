1c8929ac601a792411666a74a645b29b924c3240
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-18 18:24:14 +0200

    added  outLet property  and context

--- Exercises/src/pages/BookLayout.jsx
+++ Exercises/src/pages/BookLayout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { Link, Outlet } from "react-router-dom";
 const BookLayout = () => {
-  return <div>BookLayout</div>;
+  return (
+    <>
+      <h1>These is the header</h1>
+
+      <p>
+        <Link to="/books/1"> Book 1 </Link>
+      </p>
+      <p>
+        <Link to="/books/2"> Book 2 </Link>
+      </p>
+      <p>
+        <Link to="/books/3"> Book 3 </Link>
+      </p>
+      <h1>these is the footer </h1>
+      <Outlet context={{ hello: "helloword" }} />
+    </>
+  );
 };
 
 export default BookLayout;
