46fa5aeacc7e35b190d573009b78bb7525694f49
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-19 13:38:25 +0200

    useSearchParams  hook

--- Exercises/src/pages/BookLayout.jsx
+++ Exercises/src/pages/BookLayout.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 const BookLayout = () => {
+  const [number, setNumber] = useState("");
+  const [searchParm, setSearchParm] = useSearchParams({ n: 3 });
   return (
     <>
       <h1>These is the header</h1>
@@ -12,10 +14,14 @@ const BookLayout = () => {
         <Link to="/books/2"> Book 2 </Link>
       </p>
       <p>
+        <Link to={`books/${number}`}> Book {number}</Link>
+      </p>
+      <p>
         <Link to="/books/3"> Book 3 </Link>
       </p>
       <h1>these is the footer </h1>
       <Outlet context={{ hello: "helloword" }} />
+      <input type="number" onChange={(e) => e.target.value} />
     </>
   );
 };
