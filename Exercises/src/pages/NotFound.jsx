7a3a6a2556f95e892c86bcbabc68ca8cb0052e61
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-19 13:39:16 +0200

    added useNavigate hook for better use

--- Exercises/src/pages/NotFound.jsx
+++ Exercises/src/pages/NotFound.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigator = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigator("/");
+    }, 1000);
+  }, []);
+  return (
+    <>
+      <div>NotFound</div>
+      {/* instead directing a user direct a user to these
+      page outomatically you can use these  using useNavigator can directly
+      put you to these like home page and these can work like in case of login
+      page */}
+    </>
+  );
 };
 
 export default NotFound;
