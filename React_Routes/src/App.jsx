7a17b85ce0252f944923e352079acd55f80de064
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-20 12:00:11 +0200

    added necessary imports

--- React_Routes/src/App.jsx
+++ React_Routes/src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetails";
+
+import "./server";
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <header>
-          <Link to="/">#VANLIFE</Link>
-          <nav>
-            <Link to="/about">About</Link>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
