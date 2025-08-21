45f360856b6590e30e9acc1c3a46eb87b700090b
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-21 09:43:00 +0200

    these to be done on 21th

--- React_Routes/src/pages/VanDetails.jsx
+++ React_Routes/src/pages/VanDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 export default function VanDetail() {
   const params = useParams();
   console.log(params);
+  console.log(params);
 
   return <h1>Van detail page goes here</h1>;
 }
