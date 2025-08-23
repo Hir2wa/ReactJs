143e95f8cb14ac2713d5b6ae4f5920fe1821c6a4
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-23 20:29:17 +0200

    added new component

---
+++ React_Routes/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { FaCheck, FaTimes, FaInfo } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
