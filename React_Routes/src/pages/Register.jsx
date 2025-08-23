ab5517bcbb6b91f65452899dca286d06ec5a83ae
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-23 21:00:53 +0200

    defined the email pattern

--- React_Routes/src/pages/Register.jsx
+++ React_Routes/src/pages/Register.jsx
@@ -3,6 +3,8 @@ import { useState, useRef, useEffect } from "react";
 import { FaCheck, FaTimes, FaInfo } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Register = () => {
+  const user = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+  const email = /^(?=.*[a-z])(?=.*[a-z])(?=.*[!@#%]).{8,24}$/;
   return <div>Register</div>;
 };
 
