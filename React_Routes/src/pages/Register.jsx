c50c150533b5bb393eb26b003569bb9bdb199d2c
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-25 11:06:47 +0200

    diffined all states

--- React_Routes/src/pages/Register.jsx
+++ React_Routes/src/pages/Register.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { FaCheck, FaTimes, FaInfo } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const user = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-const email = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#%]).{8,24}$/;
+const userRegex = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const emailRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#%]).{8,24}$/;
 const Register = () => {
   const errRef = useRef();
   const userRef = useRef();
 
-  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
   const [validName, setValidName] = useState();
   const [userFocus, setUserFocus] = useState();
 
@@ -22,6 +22,17 @@ const Register = () => {
 
   const [errMsg, setErrMsg] = useState();
   const [success, setSuccess] = useState();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const result = userRegex.test(user);
+    console.log(result);
+    console.log(user);
+    setUser(setValidName);
+  }, []);
 
   return <div>Register</div>;
 };
