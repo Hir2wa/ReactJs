e3ce1efe47843b906f6a6bbe4d2debd394e00b62
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-25 10:58:06 +0200

    defined all states to be used

--- React_Routes/src/pages/Register.jsx
+++ React_Routes/src/pages/Register.jsx
@@ -6,6 +6,23 @@ const user = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const email = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#%]).{8,24}$/;
 const Register = () => {
   const errRef = useRef();
+  const userRef = useRef();
+
+  const [name, setName] = useState("");
+  const [validName, setValidName] = useState();
+  const [userFocus, setUserFocus] = useState();
+
+  const [pwd, setPwd] = useState();
+  const [validPwd, setValidPwd] = useState();
+  const [pwdFocus, setPwdFocus] = useState();
+
+  const [matchPwd, setMatchPwd] = useState();
+  const [validMatch, setValidMatch] = useState();
+  const [matchFocus, setMatchPwdFocus] = useState();
+
+  const [errMsg, setErrMsg] = useState();
+  const [success, setSuccess] = useState();
+
   return <div>Register</div>;
 };
 
