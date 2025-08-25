4c69a6685ed1b316bbc0b2c42e724ddb09c16293
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-25 10:47:26 +0200

    defined useRef state to show to the error on the screen as the user type

--- React_Routes/src/pages/Register.jsx
+++ React_Routes/src/pages/Register.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useState, useRef, useEffect } from "react";
 import { FaCheck, FaTimes, FaInfo } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const user = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const email = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#%]).{8,24}$/;
 const Register = () => {
-  const user = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-  const email = /^(?=.*[a-z])(?=.*[a-z])(?=.*[!@#%]).{8,24}$/;
+  const errRef = useRef();
   return <div>Register</div>;
 };
 
