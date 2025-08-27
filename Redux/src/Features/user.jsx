4eb47143d038e07e70adc6aba3a6140d8e810872
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-27 13:27:35 +0200

    defined the login action

--- Redux/src/Features/user.jsx
+++ Redux/src/Features/user.jsx
@@ -1,1 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+//these help use the create our Reducer in eazy way
+const userSlice = createSlice({
+  name: "user",
+  initialState: { name: "", age: 0, email: "" },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
