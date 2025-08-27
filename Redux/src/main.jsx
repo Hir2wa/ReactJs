2763e0f98dda91d8d188fdab90b58903f4e57827
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-27 12:44:15 +0200

    started redux course

---
+++ Redux/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore();
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
