d32f56dbcaead45204ae6b5f52247cbb7f45355a
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-28 12:34:23 +0200

    imported new changes

--- React_Routes/src/App.jsx
+++ React_Routes/src/App.jsx
@@ -5,28 +5,39 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetails";
-
+import Register from "./pages/Register";
 import "./server";
+import Tabs from "./pages/Tabs";
+import CountDown from "./pages/CountDown";
+import ThemeContext from "./pages/ThemeContext";
+import ThemeButton from "./pages/ThemeButton";
 
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <header>
+    //     <Link className="site-logo" to="/">
+    //       #VanLife
+    //     </Link>
+    //     <nav>
+    //       <Link to="/about">About</Link>
+    //       <Link to="/vans">Vans</Link>
+    //     </nav>
+    //   </header>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/vans" element={<Vans />} />
+    //     <Route path="/vans/:id" element={<VanDetail />} />
+    //   </Routes>
+    // </BrowserRouter>
+    // <Register />
+    <>
+      {/* <Tabs />
+      <CountDown /> */}
+
+      <ThemeButton />
+    </>
   );
 }
 
