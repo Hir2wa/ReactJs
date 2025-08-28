dbd3172676cfcb67ae9159774a1af0d4be6d4111
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-28 13:17:02 +0200

    added new features

--- Redux/src/Features/user.jsx
+++ Redux/src/Features/user.jsx
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 //these help use the create our Reducer in eazy way
-const userSlice = createSlice({
+export const userSlice = createSlice({
   name: "user",
-  initialState: { name: "", age: 0, email: "" },
+  initialState: { value: { name: "", age: 0, email: "" } },
   reducers: {
     login: (state, action) => {
       state.value = action.payload;
     },
   },
 });
+
+export default userSlice.reducer;
