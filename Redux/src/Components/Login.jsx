eb0a074200a54ac1c8da7ff64481faa8d9001ee7
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-29 11:38:16 +0200

    added logout feature

--- Redux/src/Components/Login.jsx
+++ Redux/src/Components/Login.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { login } from "../Features/user";
+import { login, logout } from "../Features/user";
 const Login = () => {
   const dispatch = useDispatch();
   return (
-    <button
-      onClick={() =>
-        dispatch(
-          login({
-            name: "Alain",
-            age: "2s",
-            email: "alainfabricehirwa@gmail.com",
-          })
-        )
-      }
-    >
-      Login
-    </button>
+    <>
+      <button
+        onClick={() =>
+          dispatch(
+            login({
+              name: "Alain",
+              age: "2s",
+              email: "alainfabricehirwa@gmail.com",
+            })
+          )
+        }
+      >
+        Login
+      </button>
+
+      <button onClick={() => dispatch(logout())}>Logout</button>
+    </>
   );
 };
 
