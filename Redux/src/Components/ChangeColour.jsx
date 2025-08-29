d15595497d3421130af963042f4a4191a086f7de
Hir2wa <alainfabricehirwa@gmail.com>
2025-08-29 12:43:03 +0200

    added colour component

--- Redux/src/Components/ChangeColour.jsx
+++ Redux/src/Components/ChangeColour.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ChangeColour = () => {
-  return <button>ChangeColour</button>;
+  const [color, setColor] = useState();
+  return (
+    <>
+      <input type="text" onChange={(e) => setColor(e.target.value)} />
+      <button>ChangeColour</button>)
+    </>
+  );
 };
 
 export default ChangeColour;
