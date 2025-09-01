abb4e5ea831cd3f4cdbd3606263b324062196e5f
Hir2wa <alainfabricehirwa@gmail.com>
2025-09-01 12:37:45 +0200

    added profile component

--- Redux/src/Components/Profile.jsx
+++ Redux/src/Components/Profile.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import colourChange from "./ChangeColour";
 
 const Profile = () => {
   const user = useSelector((state) => state.user.value);
-  const Theme = useSelector((state) => state.Theme.value);
+  const Theme = useSelector((state) => state.theme.value);
   return (
     <div style={{ color: Theme }}>
       <h1>Profile Page</h1>
       <p>Name {user.name}</p>
       <p>Age {user.age}</p>
       <p>Email {user.email}</p>
-      <colourChange />
     </div>
   );
 };
