95adc6856cc26c0a7485632047b8095a2806d20b
Hir2wa <alainfabricehirwa@gmail.com>
2025-09-01 12:28:09 +0200

    added new kata

--- Codewars.js
+++ Codewars.js
@@ -1,14 +1,28 @@
-function sortArray(arr) {
-  let odds = arr.filter((num) => num % 2 !== 0).sort((a, b) => a - b);
+// function sortArray(arr) {
+//   let odds = arr.filter((num) => num % 2 !== 0).sort((a, b) => a - b);
 
-  let result = arr.map((num) => {
-    if (num % 2 !== 0) {
-      return odds.shift();
+//   let result = arr.map((num) => {
+//     if (num % 2 !== 0) {
+//       return odds.shift();
+//     }
+//     return num;
+//   });
+
+//   return result;
+// }
+
+// console.log(sortArray([9, 5, 8, 9, 0, 3]));
+function findUniq(arr) {
+  const normalize = (str) =>
+    [...new Set(str.toLowerCase().replace(/\s/g, ""))].sort().join("");
+
+  const mapped = arr.map(normalize);
+
+  for (let i = 0; i < mapped.length; i++) {
+    if (mapped.indexOf(mapped[i]) === mapped.lastIndexOf(mapped[i])) {
+      return arr[i];
     }
-    return num;
-  });
-
-  return result;
+  }
 }
 
-console.log(sortArray([9, 5, 8, 9, 0, 3]));
+console.log(findUniq(["Aa", "aaa", "aaaaa", "BbBb", "Aaaa", "AaAaAa", "a"]));
--- Redux/src/Components/Profile.jsx
+++ Redux/src/Components/Profile.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import colourChange from "./ChangeColour";
 
 const Profile = () => {
   const user = useSelector((state) => state.user.value);
+  const Theme = useSelector((state) => state.Theme.value);
   return (
-    <>
+    <div style={{ color: Theme }}>
       <h1>Profile Page</h1>
       <p>Name {user.name}</p>
       <p>Age {user.age}</p>
       <p>Email {user.email}</p>
-    </>
+      <colourChange />
+    </div>
   );
 };
 
